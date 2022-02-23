7f02689fd74671f356a6ee77b7957ca896c9ca7a
roycobing <79212199+roycobing@users.noreply.github.com>
2022-02-24 01:52:22 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -351,54 +351,7 @@ $('#rootcause-down').click(function () {
   $('.rootcause-button').show();
 });
 
-// CloudDispatch 3030
-$(function () {
-  var stickyHeaderTop = $('#stickytypeheader').offset().top;
 
-  $(window).scroll(function () {
-    if ($(window).scrollTop() > stickyHeaderTop) {
-      $('#stickytypeheader').addClass('fixed');
-    } else {
-      $('#stickytypeheader').removeClass('fixed');
-    }
-  });
-});
-
-$(function highlightOnScrollElements() {
-  var sections = $(".section");
-  if (!sections.length) {
-    return;
-  }
-  const topGap = Math.max(
-    sections[0].getBoundingClientRect().top,
-    $(sections[0]).offset().top,
-    0
-  );
-
-  $(window)
-    .on("scroll", function (e) {
-      var el = sections.filter(function (i, el) {
-        return (
-          el.getBoundingClientRect().bottom - topGap >=
-          parseInt($(el).css("height"))
-        );
-      });
-
-      var sectionId = el.prev().is(sections)
-        ? $(el).prev(".section").attr("id")
-        : sections.eq(-1).attr("id");
-
-      $(".scrollHighlight").each(function (_, el) {
-        const href = $(el).attr("href");
-        if (href.includes(sectionId)) {
-          $(el).addClass("selected");
-        } else {
-          $(el).removeClass("selected");
-        }
-      });
-    })
-    .scroll();
-});
 
 $('#rootResources').click(function () {
   $('.fender-3030-body').hide();
